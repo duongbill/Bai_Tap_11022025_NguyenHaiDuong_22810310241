facd38030bc234a06e6dfb3aabf6e75b5d471f60
duongbill <billduongg@gmail.com>
2025-02-11 13:48:12 +0700

    duong

--- my-app/App.js
+++ my-app/App.js
@@ -1,59 +1,56 @@
-import React from 'react';
-import { View, Text, FlatList, SectionList, Alert, StyleSheet } from 'react-native';
-
-// Danh sách sản phẩm
-const products = [
-  { id: '1', name: 'Product A', price: '10.00' },
-  { id: '2', name: 'Product B', price: '15.00' },
-  { id: '3', name: 'Product C', price: '20.00' },
-];
-
-// Danh sách nhóm sản phẩm
-const groupedProducts = [
-  { title: 'Category A', data: ['Product A1', 'Product A2', 'Product A3'] },
-  { title: 'Category B', data: ['Product B1', 'Product B2'] },
-  { title: 'Category C', data: ['Product C1', 'Product C2', 'Product C3'] },
-];
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const App = () => {
-  const renderProductItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Text onPress={() => handlePress(item.name)}>{item.name}</Text>
-      <Text>{item.price}</Text>
-    </View>
-  );
-
-  const handlePress = (name) => {
-    Alert.alert(name);
-  };
-
-  const renderSectionItem = ({ item }) => (
-    <Text style={styles.item} onPress={() => Alert.alert(item)}>
-      {item}
-    </Text>
-  );
-
-  const renderSectionHeader = ({ section: { title } }) => (
-    <Text style={styles.header}>{title}</Text>
-  );
+  const [backgroundColor, setBackgroundColor] = useState("green");
 
   return (
-    <View style={styles.container}>
-      <Text style={[styles.title, styles.marginTop]}>Danh sách sản phẩm</Text>
-      <FlatList
-        data={products}
-        keyExtractor={(item) => item.id}
-        renderItem={renderProductItem}
-        contentContainerStyle={styles.listMargin} // Cách lề trên và dưới cho FlatList
-      />
-      <Text style={[styles.title, styles.marginTop]}>Danh sách nhóm sản phẩm</Text>
-      <SectionList
-        sections={groupedProducts}
-        keyExtractor={(item, index) => item + index}
-        renderItem={renderSectionItem}
-        renderSectionHeader={renderSectionHeader}
-        contentContainerStyle={styles.listMargin} // Cách lề trên và dưới cho SectionList
-      />
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text
+        style={[
+          styles.text,
+          { color: backgroundColor === "yellow" ? "black" : "white" },
+        ]}
+      >
+        {backgroundColor.toUpperCase()}
+      </Text>
+
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "green" }]}
+        onPress={() => setBackgroundColor("green")}
+      >
+        <Text style={styles.buttonText}>GREEN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "blue" }]}
+        onPress={() => setBackgroundColor("blue")}
+      >
+        <Text style={styles.buttonText}>BLUE</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "brown" }]}
+        onPress={() => setBackgroundColor("brown")}
+      >
+        <Text style={styles.buttonText}>BROWN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "yellow" }]}
+        onPress={() => setBackgroundColor("yellow")}
+      >
+        <Text style={[styles.buttonText, { color: "black" }]}>YELLOW</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "red" }]}
+        onPress={() => setBackgroundColor("red")}
+      >
+        <Text style={styles.buttonText}>RED</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: "black" }]}
+        onPress={() => setBackgroundColor("black")}
+      >
+        <Text style={[styles.buttonText, { color: "white" }]}>BLACK</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -61,37 +58,26 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
   },
-  title: {
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+  button: {
+    width: 350,
+    padding: 15,
+    margin: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-  marginTop: {
-    marginTop: 30, // Điều chỉnh giá trị này để thay đổi khoảng cách lề trên của phần tử
-  },
-  itemContainer: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    marginVertical: 5,
-  },
-  item: {
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    marginVertical: 5,
-  },
-  header: {
-    padding: 10,
-    backgroundColor: '#f4f4f4',
-    fontWeight: 'bold',
-    marginVertical: 5,
-  },
-  listMargin: {
-    marginVertical: 10,
+    fontWeight: "bold",
+    color: "white",
   },
 });
 
